2b9169f714a798a8cbe7d0bb57f4cafa082a2544
varungazula <you@example.com>
2024-01-22 01:21:15 +0530

    Changes after merging

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import Navbar from "./navbar";
-import SliderOneImage from "./SliderOneImage";
-import SliderOneContent from "./SliderOneContent";
-import HomePartThree from "./homePartThree";
-import HomePartFour from "./homePartFour";
 import Home from "./Home";
 import TermsConditions from "./termsConditions";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
@@ -12,41 +8,22 @@ import RefundReturn from "./refundReturnPolicy";
 import ShippingPolicy from "./ShippingPolicy";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Router basename = "/" >
-        <
-        Navbar / >
-        <
-        Routes >
-        <
-        Route exact path = "/"
-        element = { < Home / > }
-        /> <
-        Route path = "/About" / >
-        <
-        Route path = "/TermsConditions"
-        element = { < TermsConditions / > }
-        /> <
-        Route path = "/RefundReturnPolicy"
-        element = { < RefundReturn / > }
-        /> <
-        Route path = "/ShippingPolicy"
-        element = { < ShippingPolicy / > }
-        /> <
-        Route path = "/PrivacyPolicy"
-        element = { < PrivacyPolicy / > }
-        /> <
-        Route path = "/Contact"
-        element = { < TermsConditions / > }
-        /> <
-        /Routes> <
-        /Router> <
-        HomePartFour / >
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+<Router basename="/">
+        <Navbar/>
+        <Routes> 
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/About" />
+          <Route path="/TermsConditions" element={<TermsConditions/>}/>
+          <Route path="/RefundReturnPolicy" element={<RefundReturn/>}/>
+          <Route path="/ShippingPolicy" element={<ShippingPolicy/>}/>
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy/>}/>
+          <Route path="/Contact" element={<TermsConditions/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import SliderOneImage from "./SliderOneImage";
 import SliderOneContent from "./SliderOneContent";
 import HomePartThree from "./homePartThree";
+import HomePartFour from "./homePartFour";
+
 const Home = () => {
   return (
     <div className="home">
       <SliderOneImage/>
       <SliderOneContent/>
       <HomePartThree/>
+      <HomePartFour/>
     </div>
   );
 };
