480e017d37f3a0b557fe1391844e5477b2a0deb2
Sourav Mudaliar <sourav.mudaliar@visitor.upm.com>
2024-02-01 08:18:44 +0530

    New Changes in Landing Page

--- src/SliderTwoContent.jsx
+++ src/SliderTwoContent.jsx
@@ -4,6 +4,8 @@ const SliderTwoContent = () => {
   return (
     <div className='overlay-text'>
         <h1>CUDWARM FASHION WEEK</h1>
+        <p>Elegance Redefined</p>
+        <hr/>
     </div>
   );
 };
