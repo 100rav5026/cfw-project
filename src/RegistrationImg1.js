573ccf460adc330bcd97a0d8549dd57d87885a4d
varungazula <you@example.com>
2024-02-11 15:46:32 +0530

    responsive done

--- src/RegistrationImg1.js
+++ src/RegistrationImg1.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import RegistrationImage from './images/registrationPage1.jpeg'
 import RegistrationImg2 from './images/RegistrationImage6.png';
-import RegistrationImg from './Registrationmg';
-import RegistrationPageContent from './RegistrationPageContent';
-import RegistrationImageTwo from './RegistrationImage2';
 
 const RegistrationImageOne = () => {
-    return ( <div  >
-            <div className = "image-container-1">
-        <img src = { RegistrationImage } alt = "Background" className = "background-image-1"/>
-        <img src = { RegistrationImg2 } alt = "Background" className = "background-image-2"/>
-</div>
-         <RegistrationPageContent/>
-        <RegistrationImg/>
-        <RegistrationImageTwo/>
+    return (
+        <div className='reg-images-1' >
+            <div className="image-container-1">
+                <img src={RegistrationImage} alt="Background" className="background-image-1" />
+                <img src={RegistrationImg2} alt="Background" className="background-image-2" />
+            </div>
         </div>
-        
+
 
     );
 };
